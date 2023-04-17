cd09029e60763b131048ecaaf5de9833107761df
gaboliveiradev <g38789138@gmail.com>
2023-04-17 08:24:56 -0300

    criando as principais rotas do app

--- App/rotas.php
+++ App/rotas.php
@@ -9,6 +9,36 @@ $parse_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($parse_uri) {
 
+    // Criar conta de um usuário / correntista.
+    // [OK] http://localhost:8000/correntista/save
+    case "/correntista/save":
+
+    break;
+    
+    // Exibe o extrato referente a uma conta.
+    // [OK] http://localhost:8000/conta/extrato
+    case "/conta/extrato":
+
+    break;
+
+    // Enviar pix de uma determinada conta.
+    // [OK] http://localhost:8000/conta/pix/enviar
+    case "/conta/pix/enviar":
+
+    break;
+
+    // Receber pix de uma determinada conta.
+    // [OK] http://localhost:8000/conta/pix/receber
+    case "/conta/pix/receber":
+
+    break;
+    
+    // Logar em uma conta já existente
+    // [OK] http://localhost:8000/correntista/entrar
+    case "/correntista/entrar":
+
+    break;
+
     default:
         http_response_code(403);
     break;
